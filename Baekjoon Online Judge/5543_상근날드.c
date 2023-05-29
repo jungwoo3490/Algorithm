7d37cd5f8327daed97e45114b0ebb26182b5ef12
jungwoo3490 <jungwoo3490@naver.com>
2023-05-29 23:48:43 +0900

    2023.05.29 BOJ solved (bronze)

--- Baekjoon Online Judge/5543_상근날드.c
+++ Baekjoon Online Judge/5543_상근날드.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-int main()
-{
+int main() {
     int i = 0;
-    int a[3] = {0};
-    int b[2] = {0};
-    for (i = 0; i < 3; i++)
-    {
+    int a[3] = { 0 };
+    int b[2] = { 0 };
+    for (i = 0; i < 3; i++) {
         scanf("%d", &a[i]);
     }
-    for (i = 0; i < 2; i++)
-    {
+    for (i = 0; i < 2; i++) {
         scanf("%d", &b[i]);
     }
     int mina = a[0];
-    for (i = 1; i < 3; i++)
-    {
+    for (i = 1; i < 3; i++) {
         if (a[i] < mina)
             mina = a[i];
     }
