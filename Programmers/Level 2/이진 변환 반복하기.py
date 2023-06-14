28d20f1816c6c93c65f69fdad625c3a9dab9edbe
jungwoo3490 <jungwoo3490@naver.com>
2023-06-14 23:13:17 +0900

    2023.06.14 programmers solved (Lv2)

---
+++ Programmers/Level 2/이진 변환 반복하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    cnt_binary = 0
+    cnt_zero = 0
+    while 1:
+        if s == "1":
+            break
+        cnt_binary += 1
+        cnt_zero += s.count("0")
+        s = s.replace("0", "")
+        len_s = len(s)
+        s = bin(len_s)[2:]
+    answer.append(cnt_binary)
+    answer.append(cnt_zero)
+    return answer
