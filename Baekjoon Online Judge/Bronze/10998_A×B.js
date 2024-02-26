0397a58d8a677ba7972c0a58e4857c410669dc4f
jungwoo3490 <jungwoo3490@naver.com>
2024-02-27 02:40:15 +0900

    2024.02.27 BOJ solved (bronze)

---
+++ Baekjoon Online Judge/Bronze/10998_A×B.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const numbers = input[0].split(" ");
+
+console.log(Number(numbers[0]) * Number(numbers[1]));
