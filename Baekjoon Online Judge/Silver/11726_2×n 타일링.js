2ca0170e07c9da55f910da7d1560d4e98d3ea406
jungwoo3490 <jungwoo3490@naver.com>
2024-03-26 11:32:06 +0900

    2024.03.25 BOJ solved (silver)

---
+++ Baekjoon Online Judge/Silver/11726_2×n 타일링.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input[0]);
+let dp = [0, 1, 2];
+
+for (i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
