c89d5a9ce76ddddc30fd33a6b376226dd1d1c2f8
jungwoo3490 <jungwoo3490@naver.com>
2025-09-09 04:20:20 +0900

    2025.09.09 BOJ solved (silver)

---
+++ Baekjoon Online Judge/Silver/2583_영역 구하기.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let count;
+
+function dfs(y, x) {
+  visited[y][x] = 1;
+  let dy = [-1, 0, 1, 0];
+  let dx = [0, 1, 0, -1];
+
+  for (let i = 0; i < 4; i++) {
+    ny = y + dy[i];
+    nx = x + dx[i];
+
+    if (ny < 0 || ny >= M || nx < 0 || nx >= N) {
+      continue;
+    }
+
+    if (visited[ny][nx] === 1) {
+      continue;
+    }
+
+    if (map[ny][nx] === 0) {
+      continue;
+    }
+
+    dfs(ny, nx);
+    count += 1;
+  }
+}
+
+const [M, N, K] = input[0].split(" ").map(Number);
+
+let map = Array.from({ length: M }, () => Array(N).fill(1));
+let visited = Array.from({ length: M }, () => Array(N).fill(0));
+
+for (let i = 1; i < input.length; i++) {
+  const [x1, y1, x2, y2] = input[i].split(" ").map(Number);
+
+  for (let j = y1; j < y2; j++) {
+    for (let k = x1; k < x2; k++) {
+      map[j][k] = 0;
+    }
+  }
+}
+
+let resArr = [];
+
+for (let i = 0; i < M; i++) {
+  for (let j = 0; j < N; j++) {
+    if (map[i][j] === 1 && visited[i][j] === 0) {
+      count = 1;
+      dfs(i, j);
+      resArr.push(count);
+    }
+  }
+}
+
+console.log(resArr.length);
+console.log(resArr.sort((a, b) => a - b).join(" "));
