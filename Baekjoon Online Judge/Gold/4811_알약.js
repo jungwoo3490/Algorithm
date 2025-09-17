71d9547127f4db6728134f3787d1bc4072c7d2ef
jungwoo3490 <jungwoo3490@naver.com>
2025-09-18 06:17:30 +0900

    주석 추가

--- Baekjoon Online Judge/Gold/4811_알약.js
+++ Baekjoon Online Judge/Gold/4811_알약.js
@@ -8,21 +8,28 @@ for (let i = 0; i < input.length - 1; i++) {
   let dp = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
 
   function total(one, half) {
+    // 한가지 문자열 완성!! 경우의 수 1 추가
     if (one === 0 && half === 0) {
       return 1;
     }
 
+    // dp에 값 있으면 그거 반환
     if (dp[one][half] !== 0) {
       return dp[one][half];
     }
 
     let tot;
 
+    // 하나짜리 알약이 없으면
     if (one === 0) {
+      // 반개짜리 먹는 방법 뿐
       tot = total(one, half - 1);
+      // 반개짜리 알약이 없으면
     } else if (half === 0) {
+      // 하나짜리 먹는 방법 뿐
       tot = total(one - 1, half + 1);
     } else {
+      // 아니면 둘 다 할 수 있다
       tot = total(one - 1, half + 1) + total(one, half - 1);
     }
     dp[one][half] = tot;
