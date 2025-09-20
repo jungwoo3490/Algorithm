5ea558cafb3df73c0d3e30a769352bc01535acb0
jungwoo3490 <jungwoo3490@naver.com>
2025-09-21 01:17:22 +0900

    코드 오류 수정

--- Baekjoon Online Judge/Gold/12865_평범한 배낭.js
+++ Baekjoon Online Judge/Gold/12865_평범한 배낭.js
@@ -4,29 +4,21 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let [N, K] = input[0].split(" ").map(Number);
 let dp = Array(K + 1).fill(0);
 
-let items = {};
+let items = [];
 
+// 하나씩만 골라야 하는 거니까 중복을 무시하면 안된다.
 for (let i = 1; i <= N; i++) {
   let [W, V] = input[i].split(" ").map(Number);
-  if (items[W]) {
-    if (V > items[W]) {
-      items[W] = V;
-    } else {
-      continue;
-    }
-  } else {
-    items[W] = V;
-  }
+  items.push([W, V]);
 }
 
-const weights = Object.keys(items).map(Number);
-
 // 중복 허용하지 않았으므로 아이템을 외부 루프에서 순회. 아이템 순서대로 하나씩 dp 배열을 업뎃한다.
-for (let i = 0; i < weights.length; i++) {
+for (let i = 0; i < items.length; i++) {
+  let [W, V] = items[i];
   // 중복 허용하지 않았으므로 아이템은 역순비교 해야한다. 정순비교는 중복 미허용을 보장 못함.
-  for (let j = K; j >= weights[i]; j--) {
-    if (dp[j - weights[i]] + items[weights[i]] > dp[j]) {
-      dp[j] = dp[j - weights[i]] + items[weights[i]];
+  for (let j = K; j >= W; j--) {
+    if (dp[j - W] + V > dp[j]) {
+      dp[j] = dp[j - W] + V;
     }
   }
 }
